ac96c2fba178cd1bf94bb3e36cda266bd3f88cdc
Nilanjana1234 <88381697+Nilanjana1234@users.noreply.github.com>
2021-09-09 03:35:50 +0530

    Add files via upload

--- src/app/controllers/user.controller.js
+++ src/app/controllers/user.controller.js
@@ -1,5 +1,5 @@
 var User = require("../../../models/users.model");
-
+var auth=require("../controllers/auth.controller")
 exports.create = (req, res) => {
   // Validate request
   if (!req.body) {
@@ -14,8 +14,8 @@ exports.create = (req, res) => {
     mobile: req.body.mobile,
     email: req.body.email,
     password: req.body.password,
+    userRole: req.body.userRole,
   });
-
   // Save User in the database
   User.create(user, (err, data) => {
     if (err)
@@ -39,6 +39,30 @@ exports.findAll = (req, res) => {
   });
 };
 
+// Retrieve all Customer from the database.
+exports.allCustomer = (req, res) => {
+  User.findCustomer((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving Customers."
+      });
+    else res.send(data);
+  });
+};
+
+// Retrieve all Dealers from the database.
+exports.allDealer = (req, res) => {
+  User.findDealer((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving Customers."
+      });
+    else res.send(data);
+  });
+};
+
 // Find a single User with a userId
 exports.findOne = (req, res) => {
   User.findById(req.params.userId, (err, data) => {
@@ -50,6 +74,23 @@ exports.findOne = (req, res) => {
       } else {
         res.status(500).send({
           message: "Error retrieving User with id " + req.params.userId
+        });
+      }
+    } else res.send(data);
+  });
+};
+
+// Find a single User with a User Email
+exports.findbyEmail = (req, res) => {
+  User.findByEmail(req.params.userEmail, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not Found User with Email ${req.params.userEmail}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Error Retrieving User with Email " + req.params.userEmail
         });
       }
     } else res.send(data);
@@ -84,6 +125,61 @@ exports.update = (req, res) => {
   );
 };
 
+// Activate a User identified by the userId in the request
+exports.activeUser = (req, res) => {
+  // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+  User.activeUser(
+    req.params.userId,
+    new User(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found User with id ${req.params.userId}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Error updating User with id " + req.params.userId
+          });
+        }
+      } else res.send(data);
+    }
+  );
+};
+
+// InActivate a User identified by the userId in the request
+exports.inActiveUser = (req, res) => {
+  // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+  User.inActiveUser(
+    req.params.userId,
+    new User(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found User with id ${req.params.userId}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Error updating User with id " + req.params.userId
+          });
+        }
+      } else res.send(data);
+    }
+  );
+};
+
+
 // Delete a User with the specified userId in the request
 exports.delete = (req, res) => {
   User.remove(req.params.userId, (err, data) => {
@@ -111,4 +207,16 @@ exports.deleteAll = (req, res) => {
       });
     else res.send({ message: `All Users were deleted successfully!` });
   });
+};
+
+exports.adminBoard = (req, res) => {
+  res.status(200).send("Admin Content.");
+};
+
+exports.dealerBoard = (req, res) => {
+  res.status(200).send("Dealer Content.");
+};
+
+exports.customerBoard = (req, res) => {
+  res.status(200).send("Customer Content.");
 };
