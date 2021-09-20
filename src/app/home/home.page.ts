6778bcda58b70281155dedc8e86f069a317ecec0
kamana44pandey99 <62660333+kamana44pandey99@users.noreply.github.com>
2021-09-20 11:14:45 +0530

    Add files via upload

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NavParams } from '@ionic/angular';
-// import { GooglePlus } from '@ionic-native/google-plus/ngx';
-// import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
 import { APIService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
-  providers: [NavParams],
-  // providers: [NavParams,GooglePlus,Facebook]
+  providers: [NavParams,GooglePlus,Facebook]
 })
 export class HomePage implements OnInit {
 
@@ -21,8 +20,8 @@ export class HomePage implements OnInit {
     public navParams: NavParams,
     private router: Router,
     private activateRoute: ActivatedRoute,
-    // private googlePlus: GooglePlus,
-    // private fb: Facebook
+    private googlePlus: GooglePlus,
+    private fb: Facebook
   ) {}
 
   ngOnInit() {
@@ -37,37 +36,37 @@ export class HomePage implements OnInit {
   this.router.navigate(['signup', {role}]);
 }
 
-// googleLogin(role){
-//   this.googlePlus.login({})
-//   .then(res => {
-//     console.log(res);
-//     console.log('Logged into Facebook!', res);
-//     this.apiService.create(res).toPromise().then((a)=>{
-//     console.log('Logged into Facebook!', a);
+googleLogin(role){
+  this.googlePlus.login({})
+  .then(res => {
+    console.log(res);
+    console.log('Logged into Facebook!', res);
+    this.apiService.create(res).toPromise().then((a)=>{
+    console.log('Logged into Facebook!', a);
 
-//   }).catch(err => {
-//     console.log('Error logging into Facebook', err);
-//   });
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
-// }
+  }).catch(err => {
+    console.log('Error logging into Facebook', err);
+  });
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
 
-// facebookLogin(role){
-//   this.fb.login(['public_profile', 'user_friends', 'email'])
-//   .then((res: FacebookLoginResponse) => {
-//     console.log('Logged into Facebook!', res);
-//     this.apiService.create(res).toPromise().then((a)=>{
-//     console.log('Logged into Facebook!', a);
+facebookLogin(role){
+  this.fb.login(['public_profile', 'user_friends', 'email'])
+  .then((res: FacebookLoginResponse) => {
+    console.log('Logged into Facebook!', res);
+    this.apiService.create(res).toPromise().then((a)=>{
+    console.log('Logged into Facebook!', a);
 
-//   }).catch(err => {
-//     console.log('Error logging into Facebook', err);
-//   });
-// })
-//   .catch(e => {
-//     console.log('Error logging into Facebook', e);
-//   });
-// }
+  }).catch(err => {
+    console.log('Error logging into Facebook', err);
+  });
+})
+  .catch(e => {
+    console.log('Error logging into Facebook', e);
+  });
+}
 
 }
